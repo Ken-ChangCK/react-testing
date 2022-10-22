751384de8602f0843190dfebaccb1a8bfb6002ac
kenChangKKday <kem.chang@kkday.com>
2022-10-22 18:51:01 +0800

    feat: greet test

---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,25 @@
+// import { render, screen } from "@testing-library/react";
+
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./greet";
+
+// test("Greet renders correctly", () => {
+//   render(<Greet />);
+//   const testElement = screen.getByText(/hello/i);
+
+//   expect(testElement).toBeInTheDocument();
+// });
+
+describe("Greet", () => {
+  test("Green render correctly", () => {
+    render(<Greet />);
+    const text = screen.getByText("Hello");
+    expect(text).toBeInTheDocument();
+  });
+
+  test("Greet renders width a name", () => {
+    render(<Greet name="ABC" />);
+    const text = screen.getByText("Hello ABC");
+    expect(text).toBeInTheDocument();
+  });
+});
